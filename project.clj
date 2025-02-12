0a88fb003f36292b3ed3095d998fe35a95cf2e4c
Vale <valerauko@gmail.com>
2025-02-12 23:41:32 +0900

    Bump

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject net.valerauko/harinezumi "1.0.0"
+(defproject net.valerauko/harinezumi "1.1.0"
   :description "Clojure test helper to check ex-info details"
   :url "https://github.com/valerauko/harinezumi"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :scm {:name "git"
         :url "https://github.com/valerauko/harinezumi"
-        :tag "v1.0.0"}
+        :tag "v1.1.0"}
   :dependencies []
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.12.0"]
                                   [org.clojure/tools.namespace "1.5.0"]
